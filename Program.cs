f084d1480496952e5288a38756e7b3a8f255263d
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-07-02 00:21:44 +0900

    2023.07.02

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C_Sharp_Study {
+    public class Program {
+        static void Main() {
+            const float PI = 3.14f;
+            sbyte sb = -128;
+            int min = int.MinValue;
+            int max = int.MaxValue;
+            
+            Console.WriteLine("{0}, {1}", min, max);
+            Console.WriteLine("{0}", PI);
+            Console.WriteLine(100_000_000);
+            
+            Console.WriteLine(2.0 - 1.1);
+            Console.WriteLine(Convert.ToDecimal(2.0 - 1.1));
+
+            decimal d = 1.23m;
+            float p = 0f;
+            
+            Console.WriteLine(d);
+            Console.WriteLine(p);
+            
+            decimal.TryParse("1.4", out d);
+            float.TryParse("3.14", out p);
+            
+            Console.WriteLine(d);
+            Console.WriteLine(p);
+
+            int? x = 1;
+            Console.WriteLine(x);
+
+            string str = @"
+                            곰탱이
+                            냥탱이
+                          ";
+            Console.WriteLine(str);
+        }
+    }
+}
