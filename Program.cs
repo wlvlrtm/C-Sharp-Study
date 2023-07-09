b5ea04e21564fdce242ba2314ad63358dd1097d3
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-07-09 23:04:28 +0900

    2023.07.09

--- Program.cs
+++ Program.cs
@@ -2,41 +2,10 @@
 
 namespace C_Sharp_Study {
     public class Program {
-        static void Main() {
-            const float PI = 3.14f;
-            sbyte sb = -128;
-            int min = int.MinValue;
-            int max = int.MaxValue;
-            
-            Console.WriteLine("{0}, {1}", min, max);
-            Console.WriteLine("{0}", PI);
-            Console.WriteLine(100_000_000);
-            
-            Console.WriteLine(2.0 - 1.1);
-            Console.WriteLine(Convert.ToDecimal(2.0 - 1.1));
+        private static void Main() {
+            string color = "Red";
 
-            decimal d = 1.23m;
-            float p = 0f;
-            
-            Console.WriteLine(d);
-            Console.WriteLine(p);
-            
-            decimal.TryParse("1.4", out d);
-            float.TryParse("3.14", out p);
-            
-            Console.WriteLine(d);
-            Console.WriteLine(p);
-
-            int? x = 1;
-            Console.WriteLine(x);
-
-            string str = @"
-                            곰탱이
-                            냥탱이
-                          ";
-            Console.WriteLine(str);
-            Console.WriteLine("123" + 456);
-            
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color);
 
         }
     }
