a8f986c659784a939891ea710b45035e515b69a6
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-07-17 00:35:36 +0900

    2023.07.17

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace C_Sharp_Study {
     public class Program {
         private static void Main() {
-            string color = "Red";
 
-            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color);
-
+            
         }
     }
 }
