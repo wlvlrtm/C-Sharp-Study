aa97276f193c9bd277191792fbf3452b2ad980ce
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-06 00:26:39 +0900

    2023.08.06

--- Program.cs
+++ Program.cs
@@ -6,35 +6,10 @@ using System.Linq;
 namespace C_Sharp_Study {
     public class Program {
         private static void Main() {
-            Stack<string> stack = new Stack<string>();
+            Action<object> print = Console.WriteLine;
+            Func<int, int> getnum = (int x) => 123;
 
-            stack.Push("1");
-
-            List<int> lst = new List<int>();
-
-            foreach (var i in Enumerable.Range(1, 10)) {
-                lst.Add(i);
-                Console.WriteLine(i);
-            }
-
-            Dictionary<int, string> dic = new Dictionary<int, string>();
-
-            dic[0] = "test";
-            
-            Console.WriteLine(dic[0]);
-
-            bool? isNull = null;
-            
-            Console.WriteLine(isNull);
-
-            int[] nums = { 1, 2, 3 };
-            Console.WriteLine(nums.Sum());
-
-            var newNums = nums.Where(n => n > 2);
-
-            foreach (var i in newNums) {
-                Console.WriteLine(i);
-            }
+            print(getnum(3));
         }
     }
 }
