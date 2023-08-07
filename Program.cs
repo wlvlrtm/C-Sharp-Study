7004b25c53afa07b7d3faeb03dc0a7fe5491f3f8
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-07 22:50:56 +0900

    2023.08.07

--- Program.cs
+++ Program.cs
@@ -1,40 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 
 namespace C_Sharp_Study {
     public class Program {
-        private static void Main() {
-            Stack<string> stack = new Stack<string>();
-
-            stack.Push("1");
-
-            List<int> lst = new List<int>();
-
-            foreach (var i in Enumerable.Range(1, 10)) {
-                lst.Add(i);
-                Console.WriteLine(i);
-            }
-
-            Dictionary<int, string> dic = new Dictionary<int, string>();
-
-            dic[0] = "test";
-            
-            Console.WriteLine(dic[0]);
-
-            bool? isNull = null;
-            
-            Console.WriteLine(isNull);
-
-            int[] nums = { 1, 2, 3 };
-            Console.WriteLine(nums.Sum());
-
-            var newNums = nums.Where(n => n > 2);
-
-            foreach (var i in newNums) {
-                Console.WriteLine(i);
-            }
-        }
+        private static void Main() => Console.WriteLine("Hello, world!");
     }
 }
