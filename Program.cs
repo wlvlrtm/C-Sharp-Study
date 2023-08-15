0c1212155605de12c6ff858695b359ff96c5bb53
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-15 17:29:51 +0900

    2023.08.15

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 
 namespace C_Sharp_Study {
     public class Program {
-        private static void Main() => Console.WriteLine("Hello, world!");
+        private static void Main() {
+            Car cityCar = new Car("Casper");
+            Car campingCar = new Car();
+            Car sportCars = new Car(2);
+            Car electricCar = new Car("Ioniq 5");
+            
+            cityCar.Run();
+            
+            campingCar.CarName = "Perfect 1200 Platinum";
+            campingCar.Run();
+
+            sportCars[0] = "Sports Car 1";
+            sportCars[1] = "Sports Car 2";
+            
+            Console.WriteLine(sportCars[0]);
+            Console.WriteLine(sportCars[1]);
+
+            foreach (var name in sportCars) {
+                Console.WriteLine(name);
+            }
+
+            electricCar.Click += new Car.EventHandler(electricCar.Run);
+            electricCar.Click += new Car.EventHandler(electricCar.Run);
+            
+            electricCar.OnClick();
+        }
     }
 }
