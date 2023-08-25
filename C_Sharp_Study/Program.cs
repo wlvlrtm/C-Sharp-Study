e80438c6761e4797c900646a740909ce06bfa1d3
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-25 18:25:01 +0900

    2023.08.25

--- C_Sharp_Study/Program.cs
+++ C_Sharp_Study/Program.cs
@@ -1,21 +1,8 @@
 ﻿namespace C_Sharp_Study {
     static class C_Sharp_Study {
-        static (int, int) Tally() {
-            var r = (10, 20);
-            return r;
-        }
-        
         public static void Main() {
-            var tup = (1, 2, 3);
-            Console.WriteLine(tup.Item1);
             
-            var uhd = (Width: 3840, Height: 2160);
-            Console.WriteLine($"{uhd.Width}, {uhd.Height}");
 
-            var t = Tally();
-            Console.WriteLine(t.Item1);
-            
-            
         }
     }   
 }
