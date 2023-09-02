e6d0c1a5c3ef504d6fbecbde04fdbe9d8bc7d257
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-09-02 21:54:58 +0900

    2023.09.02

--- C_Sharp_Study/Program.cs
+++ C_Sharp_Study/Program.cs
@@ -1,8 +1,24 @@
-﻿namespace C_Sharp_Study {
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study {
     static class C_Sharp_Study {
         public static void Main() {
+            Thread thread = new Thread(new ThreadStart(ThreadDemo));
             
+            //Parallel.For(0, 5000_0000, (i) => { Console.WriteLine(i); });
+            
+            for (int i = 0; i <= 5000_0000; i++) {
+                Console.WriteLine(i);
+            }
+            
+            thread.Start();
+        }
 
+        public static void ThreadDemo() {
+            Console.Write("Hello, ");
+            Thread.Sleep(1000);
+            Console.WriteLine("world!");
         }
     }   
 }
