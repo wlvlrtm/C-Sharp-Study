a6d70e4b1bee699a3170b9b8821de50bc50eb5db
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-09-24 16:53:04 +0900

    2023.09.24 (FIN)

--- C_Sharp_Study/Program.cs
+++ C_Sharp_Study/Program.cs
@@ -4,15 +4,16 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Study {
     static class C_Sharp_Study {
-        public static async Task Main() {
-            var service = new WeatherForecastService();
-            var forecasts = await service.GetForecastAsync(DateTime.Now);
-            
-            Console.WriteLine("Date\tTemp. (C)\tSummary");
-            
-            foreach (var forecast in forecasts) {
-                Console.WriteLine($"{forecast.Date.ToShortDateString()}\t" + $"{forecast.TemperatureC}\t{forecast.Summary}");
-            }
+        public static void Main() {
+            int[] nums = { 1, 2, 3 };
+
+            int max = nums.Aggregate((f, s) => {
+                Console.WriteLine(f > s ? f : s);
+                Console.WriteLine("f: " + f);
+                Console.WriteLine("s: " + s);
+                return f + s;
+            });
+            Console.WriteLine(max);
         }
     }   
 }
